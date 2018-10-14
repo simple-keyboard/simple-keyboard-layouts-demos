7b601b592636daad715b195802b74e88d1ecf666
Francisco Hodge <hello@franciscohodge.com>
2018-10-14 15:31:54 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# demo_npm_simple-keyboard-layouts
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,42 @@
+/**
+ * simple-keyboard documentation
+ * https://github.com/hodgef/simple-keyboard
+ */
+
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+
+/**
+ * Available layouts
+ * https://github.com/hodgef/simple-keyboard-layouts/tree/master/src/lib/layouts
+ */
+import layout from "simple-keyboard-layouts/build/layouts/japanese";
+
+let keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button),
+  layout: layout
+});
+
+function onChange(input) {
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+
+  /**
+   * Shift functionality
+   */
+  if (button === "{lock}" || button === "{shift}") handleShiftButton();
+}
+
+function handleShiftButton() {
+  let currentLayout = keyboard.options.layoutName;
+  let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+  keyboard.setOptions({
+    layoutName: shiftToggle
+  });
+}
